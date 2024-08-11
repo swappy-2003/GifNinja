1de1f31257afe8f0353f5caabfde9374851dd54f
swappy-2003 <dubeyswapnil.123@gmail.com>
2024-08-11 21:41:40 +0530

    add the categories

--- src/Context/Gif-context.jsx
+++ src/Context/Gif-context.jsx
@@ -1,8 +1,27 @@
-import { createContext } from "react";
+import { createContext,useState, useContext } from "react";
+import { GiphyFetch } from "@giphy/js-fetch-api";
 
-const Gifcontext =createContext();
+const Gifcontext = createContext();
 
-const Gifprovider=({childern})=>{
-    return <Gifcontext.Provider>{childern}</Gifcontext.Provider>
-}
+const Gifprovider = ({ children }) => {
+  const [gifs, setgifs] = useState([]);
+  const [filter, setfilter] = useState("gifs");
+  const [favourites, setfavourites] = useState([]);
+
+  const gf = new GiphyFetch(import.meta.env.VITE_GIPHY_KEY);
+
+
+  return (
+    <Gifcontext.Provider
+      value={{ gf, gifs, setgifs, filter, setfilter, favourites }}
+    >
+      {children}
+    </Gifcontext.Provider>
+  );
+};
+
+export const GifState = () => {
+  return useContext(Gifcontext);
+};
+  
 export default Gifprovider;
