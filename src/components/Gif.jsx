d290dd2ac3a7054eb572d175b174281f6f180b88
swappy-2003 <dubeyswapnil.123@gmail.com>
2024-08-20 19:47:49 +0530

    add the scale transition

--- src/components/Gif.jsx
+++ src/components/Gif.jsx
@@ -8,10 +8,10 @@ const Gif = ({ gif, hover = true }) => {
         <img
           src={gif?.images?.fixed_width.webp}
           alt={gif?.title}
-          className="w-full object-cover rounded transition-all duration-300"
+          className={`w-full object-cover rounded transition-all duration-300 ${hover ? "group-hover:scale-105" : ""}` }
         />
         {hover && (
-          <div className=" absolute inset-0 rounded opacity-0 group-hover:opacity-100 bg-gradient-to-b from-transparent via-transparent to-black font-bold flex items-end gap-2 p-2 " > 
+          <div className=" absolute inset-0 rounded opacity-0 group-hover:opacity-100 group-hover:scale-105 bg-gradient-to-b from-transparent via-transparent to-black font-bold flex items-end gap-2 p-2 " > 
             <img src={gif?.user?.avatar_url} alt={gif?.user?.display_name} 
             
             className="h-8"/>
